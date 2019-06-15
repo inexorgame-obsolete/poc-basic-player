482518a0f3a582603a399c170b3f5bd0e62164bc
Andreas Schaeffer <hanack@nooblounge.net>
2019-06-15 14:16:24 +0200

    Added data structures for player and blocks

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,50 @@
+#include <memory>
+
+struct Player
+{
+
+	struct Position {
+		double x, y, z;
+	};
+
+	struct Dimensions {
+		double width, height, depth;
+	};
+
+	struct Orientation {
+		double yaw, pitch, roll;
+	};
+
+	Position position;
+	Dimensions dimensions;
+	Orientation orientation;
+
+};
+
+const double BLOCK_SIZE = 10.0;
+
+struct Block
+{
+
+	struct Position {
+		double x, y, z;
+	};
+
+	Position position;
+
+};
 
 int main()
 {
+	Player you = {
+		{ 0.0, 0.0, 0.0 },
+		{ 1.0, 1.0, 1.0 },
+		{ 1.0, 0.0, 0.0 }
+	};
+	Block block1 = {
+		{ 10.0, 0.0, 0.0 }
+	};
+	Block block2 = {
+		{ 0.0, 10.0, 0.0 }
+	};
 }
