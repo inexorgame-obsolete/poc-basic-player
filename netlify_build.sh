26c3765eed066182d2d87b100005d8e6ebb73498
Martin Muzatko <mmu@gps-stuttgart.de>
2019-09-10 22:42:13 +0200

    add bazel manual install

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,4 +1,7 @@
-apt install bazel
+wget https://github.com/bazelbuild/bazel/releases/download/0.29.1/bazel-0.29.1-installer-linux-x86_64.sh
+chomd +x bazel-0.29.1-installer-linux-x86_64.sh
+./bazel-0.29.1-installer-linux-x86_64.sh --user
+export PATH="$PATH:$HOME/bin"
 bazel --version
 echo "building app"
 mkdir -p ./build
